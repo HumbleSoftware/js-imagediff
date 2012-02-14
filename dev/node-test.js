f45082eb09f5046b9ca2fd61d23bea82fbf8affb
Jordan Santell <jsantell@gmail.com>
2012-02-14 00:21:15 -0500

    move dev files..

--- dev/node-test.js
+++
@@ -1,2 +0,0 @@
-var
-  imagediff = require('js-imagediff');
--- dev/test-canvas.js
+++ dev/test-canvas.js
@@ -1,16 +1,30 @@
-var Canvas = require('canvas')
-  , canvas = new Canvas(200,200)
+// Sloppy dev tests
+imageDiff = require('./imagediff.js');
+var Canvas = require('canvas') 
+  , canvas = new Canvas(200,100)
   , ctx = canvas.getContext('2d');
 
   ctx.font = '30px Impact';
   ctx.rotate(.1);
-  ctx.fillText("Awesome!", 50, 100);
+  ctx.fillText("W00t!", 50, 50);
 
-  var te = ctx.measureText('Awesome!');
-  ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-  ctx.beginPath();
-  ctx.lineTo(50, 102);
-  ctx.lineTo(50 + te.width, 102);
-  ctx.stroke();
+  var canvas2 = new Canvas(200,100)
+  , ctx2 = canvas.getContext('2d');
 
-console.log('<img src="' + canvas.toDataURL() + '" />');
+  ctx2.font = '30px Impact';
+  ctx2.rotate(.1);
+  ctx2.fillText("W00t!", 50, 50);
+
+var img = new Canvas.Image();
+
+
+tester = img
+console.log('Type of canvas: ', Object.prototype.toString.apply(tester));
+console.log('isContext: ', imageDiff.isContext(tester));
+
+console.log('isImage: ', imageDiff.isImage(tester));
+console.log('isCanvas: ', imageDiff.isCanvas(tester));
+console.log('isImageData: ', imageDiff.isImageData(tester));
+console.log('isImageType: ', imageDiff.isImageType(tester));
+
+console.log('Are canvas1 and 2 different: ', imageDiff.diff(canvas, canvas2));
