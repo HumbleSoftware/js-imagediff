83fe9d9ceb3d0d95acc9c3db9c2aaf8e6c21819e
Carl Sutherland <carl@humblesoftware.com>
2012-02-14 13:45:05 -0500

    Fix custom matcher message for node.

--- js/imagediff.js
+++ js/imagediff.js
@@ -267,6 +267,7 @@
     }
     return element;
   }
+
   jasmine = {
 
     toBeImageData : function () {
@@ -275,36 +276,36 @@
 
     toImageDiffEqual : function (expected, tolerance) {
 
-      this.message = function() {
+      if (typeof (document) !== UNDEFINED) {
+        this.message = function () {
+            div     = get('div'),
+            a       = get('div', '<div>Actual:</div>'),
+            b       = get('div', '<div>Expected:</div>'),
+            c       = get('div', '<div>Diff:</div>'),
+            diff    = imagediff.diff(this.actual, expected),
+            canvas  = getCanvas(),
+            context;
 
-        var
-          div     = get('div'),
-          a       = get('div', '<div>Actual:</div>'),
-          b       = get('div', '<div>Expected:</div>'),
-          c       = get('div', '<div>Diff:</div>'),
-          diff    = imagediff.diff(this.actual, expected),
-          canvas  = getCanvas(),
-          context;
+          canvas.height = diff.height;
+          canvas.width  = diff.width;
 
-        canvas.height = diff.height;
-        canvas.width  = diff.width;
+          context = canvas.getContext('2d');
+          context.putImageData(diff, 0, 0);
 
-        context = canvas.getContext('2d');
-        context.putImageData(diff, 0, 0);
+          a.appendChild(toCanvas(this.actual));
+          b.appendChild(toCanvas(expected));
+          c.appendChild(canvas);
 
-        a.appendChild(toCanvas(this.actual));
-        b.appendChild(toCanvas(expected));
-        c.appendChild(canvas);
+          div.appendChild(a);
+          div.appendChild(b);
+          div.appendChild(c);
 
-        div.appendChild(a);
-        div.appendChild(b);
-        div.appendChild(c);
-
-        return [
-          div,
-          "Expected not to be equal."
-        ];
-      };
+          return [
+            div,
+            "Expected not to be equal."
+          ];
+        };
+      }
 
       return imagediff.equal(this.actual, expected, tolerance);
     }
