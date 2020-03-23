f591114678934d140f72fdd44fb745ed776bdff5
Carl Sutherland <carl@humblesoftware.com>
2020-03-23 19:12:33 -0400

    Fix image includes in Karma config.

--- karma.config.js
+++ karma.config.js
@@ -3,9 +3,13 @@ module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
     plugins: ['karma-jasmine'],
+    proxies: {
+      '/images/': '/base/spec/images/'
+    },
     files: [
       './js/imagediff.js',
-      './spec/ImageDiffSpec.js'
+      './spec/ImageDiffSpec.js',
+      {'pattern': 'spec/images/*.png', 'included': false, 'served': true}
     ]
   })
 };
